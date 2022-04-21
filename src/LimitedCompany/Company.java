97c12e239006cad5c148440f117faac1d6fdecd7
Ozer <ozerknet@gmail.com>
2022-04-21 13:43:32 +0100

    Limited company package and company class

---
+++ src/LimitedCompany/Company.java
@@ -0,0 +1,29 @@
+package LimitedCompany;
+
+public class Company {
+    String companyDirectorName;
+    String companyDirectorSurname;
+
+    String companyDirectorNinoNumber;
+    String companyDirectorUtr;
+
+    String companyDirectorEmail;
+    String companyDirectorAddress;
+    long companyDirectorMobile;
+
+
+
+    String companyName;
+    int companyNumber;
+    String companyUtr;
+    String companyAuthCode;
+
+
+    String companyPayeRef;
+    String companyPayeOfficeRef;
+
+
+    int companyVatNumber;
+
+
+}
